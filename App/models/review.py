e67b3ec80d904803c859dc5c21248406b4133f24
TheLegend1011101 <111811728+TheLegend1011101@users.noreply.github.com>
2024-10-17 14:45:55 +0000

    Updates to Models

--- App/models/review.py
+++ App/models/review.py
@@ -4,6 +4,7 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500), nullable = False)
     student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable = False)
+    user_id = db.column(db.Integer, db.ForeignKey('user.id'), nullable = False)
 
     def __init__(self, text, student_id):
         self.text = text
@@ -15,7 +16,8 @@ class Review(db.Model):
             'id': self.id,
             'text': self.text,
             'student_id': self.student_id,
+            'user_id': self.user_id
         }
     
     def __repr__(self):
-        return f"id:{self.id} Content:{self.text} Student ID:{self.student_id}"
+        return f"id:{self.id} Content:{self.text} Student ID:{self.student_id} User ID:{self.user_id}"
--- App/models/student.py
+++ App/models/student.py
@@ -2,18 +2,24 @@ from App.database import db
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.Integer, unique = True, nullable = False)
     firstname =  db.Column(db.String(20), nullable=False)
     lastname = db.Column(db.String(20), nullable=False)
     reviews = db.relationship('Review', backref = 'student', lazy = True, cascade="all, delete-orphan")
+    email = db.Column(db.String(100), nullable = False, unique = True)
 
-    def __init__(self, firstname, lastname):
+    def __init__(self, firstname, lastname, email, public_id):
         self.firstname = firstname
         self.lastname = lastname
+        self.email = email
+        self.public_id = public_id
     
     def to_json(self):
         return {
             'id': self.id,
+            'public_id': self.public_id,
             'firstname': self.firstname,
             'lastname': self.lastname,
+            'email': self.email,
             'reviews': [review.to_json() for review in self.reviews] if self.reviews else []
         }
